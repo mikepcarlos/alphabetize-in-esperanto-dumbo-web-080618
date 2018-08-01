7af09ea03d2c0956288bc4e18fdd7da7a63afebb
mikepcarlos <michaelpcarlos@gmail.com>
2018-08-01 17:02:02 -0400

    alphebitze

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |phrase|
+    phrase.split("").map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+      #binding.pry
+    end
+  end
 end
